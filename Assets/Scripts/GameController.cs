8b47daad21ca051d066baf7b70e65fe2aaebb32e
Jeremy Red <zoomdude111@hotmail.com>
2017-10-13 13:43:48 -0700

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour {
+    private int playerScore;
+    private Text text;
+	// Use this for initialization
+	void Start () {
+        //set the score to zero
+        playerScore = 0;
+
+        //get the text componenet
+        text = GetComponent<Text>();
+        //set the texts text
+        text.text = "Score: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //set the text to the players score
+        text.text = "Score: " + playerScore;
+	}
+
+    public void IncreaseScore(int amount)
+    {
+        playerScore += amount;
+    }
+}
---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour {
+    private float projectileSpeed;
+    private const float DEFAULT_SPEED = 10.0f;
+	// Use this for initialization
+	void Start () {
+        //set the defualt projectile speed
+        projectileSpeed = DEFAULT_SPEED;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        //move the laser forward
+        transform.Translate(new Vector3(0, 0, -projectileSpeed));
+
+        //check to se if the laser is way out of the room
+        if (transform.position.z < -500.0f)
+            Destroy(gameObject);
+   
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if the collider is an enemy detroy it
+        if(collision.gameObject.CompareTag("enemy"))
+        {
+            //destroy the object
+            Destroy(collision.gameObject);
+
+            //increase the score by the enemys worth
+            GameObject.Find("GameController").GetComponent<GameController>().IncreaseScore(collision.gameObject.GetComponent<Enemy_Controller>().Worth);
+            //destroy self
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetSpeed(float speed = 10.0f)
+    {
+        projectileSpeed = speed;
+    }
+}
